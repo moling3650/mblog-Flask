a66360081d2b1c33d52efc8373cbd90d73dc5e61
moling <365024424@qq.com>
2016-08-01 18:07:51 +0800

    add helpers
    Paginate and set_positive_int

---
+++ app/helper.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2016-08-01 00:16:00
+# @Author  : moling (365024424@qq.com)
+# @Link    : http://qiangtaoli.com
+# @Version : $Id$
+
+
+# 网页翻页信息类
+class Paginate(object):
+
+    def __init__(self, item_count, index=1, size=10):
+        self.last = item_count // size + (1 if item_count % size > 0 else 0)  # 尾页
+        self.index = min(index, self.last) if item_count > 0 else 1           # 当前页
+        self.offset = size * (index - 1)    # 数据库查询用，偏移N个元素
+        self.limit = size                   # 一页有N个元素
+
+
+def set_positive_int(num_str, default=1):
+    try:
+        num = int(num_str)
+    except:
+        return default
+    return num if num > 0 else default
