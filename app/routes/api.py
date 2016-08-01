82b0b306483be1b458998f481414420cb8704be0
moling <365024424@qq.com>
2016-08-01 22:57:15 +0800

    update blueprints

--- app/routes/api.py
+++ app/routes/api.py
@@ -2,10 +2,13 @@
 # @Author: moling
 # @Date:   2016-07-28 23:39:14
 
-from flask import Blueprint, jsonify
-from ..models import User, Blog
+from flask import Blueprint, jsonify, request
+from ..models import User, Blog, Comment
+from ..helper import Paginate, set_positive_int
 
 api = Blueprint('api', __name__, url_prefix='/api')
+
+TABLES = dict(users=User, blogs=Blog, comments=Comment)
 
 
 @api.route('/test')
@@ -15,13 +18,25 @@ def test():
     })
 
 
-@api.route('/users')
-def api_get_users():
-    users = User.query.all()
-    return jsonify(users=[user.to_json() for user in users])
+@api.route('/user')
+def api_get_user():
+    user = User.query.get("001462522483997f2b422e199124d698ddf1179a92cf986000")
+    return jsonify(user=user.to_json())
 
 
-@api.route('/blogs')
-def api_get_blogs():
-    blogs = Blog.query.all()
-    return jsonify(blogs=[blog.to_json() for blog in blogs])
+# 取（用户、博客、评论）表的条目
+@api.route('/<tablename>')
+def api_get_items(tablename):
+    table = TABLES[tablename]
+    page = set_positive_int(request.args.get('page'))
+    size = set_positive_int(request.args.get('size'), 10)
+    item_count = table.query.count()
+    p = Paginate(item_count, page, size)
+    items = table.query.order_by(table.created_at.desc()).offset(p.offset).limit(p.limit).all()
+    return jsonify(items=[item.to_json() for item in items], page=p.__dict__)
+
+
+@api.route('/blogs/<id>/comments')
+def api_get_blog_comments(id):
+    comments = Comment.query.filter_by(blog_id=id).all()
+    return jsonify(comments=[c.to_json() for c in comments])
--- app/routes/main.py
+++ app/routes/main.py
@@ -1,33 +1,52 @@
 # -*- coding: utf-8 -*-
 # @Author: moling
 # @Date:   2016-07-28 18:08:16
-
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, request, url_for
 from ..models import Blog
+from ..helper import Paginate, set_positive_int
+
 main = Blueprint('main', __name__)
 
 
-# 网页翻页信息类
-class Page(object):
-
-    def __init__(self, item_count, index=1, size=10):
-        self.last = item_count // size + (1 if item_count % size > 0 else 0)  # 尾页
-        self.index = min(index, self.last) if item_count > 0 else 1           # 当前页
-        self.offset = size * (index - 1)    # 数据库查询用，偏移N个元素
-        self.limit = size
-
-
+# 跳转到模板首页
 @main.route('/')
 def index():
-    return redirect(url_for('.test'))
+    return redirect(url_for('.show_blogs', template='bootstrap'))
 
 
-@main.route('/test')
-def test():
-    return render_template('test.html')
+# 模板首页
+@main.route('/<template>/')
+def show_blogs(template):
+    page = set_positive_int(request.args.get('page'))
+    size = set_positive_int(request.args.get('size'), 10)
+    p = Paginate(Blog.query.count(), page, size)
+    blogs = Blog.query.order_by(Blog.created_at.desc()).offset(p.offset).limit(p.limit).all()
+    return render_template('%s-blogs.html' % template, page=p, blogs=blogs)
 
 
-@main.route('/bootstrap')
-def bootstrap():
-    blogs = Blog.query.all()
-    return render_template('bootstrap-blogs.html', page=Page(18), blogs=blogs)
+# 注册页面
+@main.route('/<template>/register')
+def register(template):
+    return render_template('%s-register.html' % template)
+
+
+# 登陆页面
+@main.route('/<template>/signin')
+def signin(template):
+    return render_template('%s-signin.html' % (template))
+
+
+# 博客页面
+@main.route('/<template>/blog/<id>')
+def show_blog(template, id):
+    return render_template('%s-blog.html' % template, blog=Blog.query.get(id))
+
+
+# @main.route('/<template>/manage')
+# def manage(template):
+#     redirect(url_for('.manage_table', template=template, tablename='blogs'))
+
+
+# @main.route('/<template>/manage/<tablename>')
+# def manage_table(template, tablename):
+#     return render_template('%s-manage.html' % (template), table=tablename)
