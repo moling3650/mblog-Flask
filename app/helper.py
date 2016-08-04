1a7a8dae8642880df3c156f2c432b342eef335a4
moling <365024424@qq.com>
2016-08-04 21:57:33 +0800

    add check functions

--- app/helper.py
+++ app/helper.py
@@ -4,7 +4,11 @@
 # @Author  : moling (365024424@qq.com)
 # @Link    : http://qiangtaoli.com
 # @Version : $Id$
+import re
 from flask import abort
+
+_RE_EMAIL = re.compile(r'^[a-zA-Z0-9\.\-\_]+\@[a-zA-Z0-9\-\_]+(\.[a-zA-Z0-9\-\_]+){1,4}$')
+_RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
 
 
 # 网页翻页信息类
@@ -17,6 +21,7 @@ class Paginate(object):
         self.limit = size                   # 一页有N个元素
 
 
+# 设置一个正整数
 def set_positive_int(num_str, default=1):
     try:
         num = int(num_str)
@@ -25,12 +30,22 @@ def set_positive_int(num_str, default=1):
     return num if num > 0 else default
 
 
+# 检查用户是否管理员
 def check_admin(user):
-    if user is None or not user.admin:
+    if user is None or not user.get('admin'):
         abort(403, 'current user must be admin')
 
 
+# 检查字符串是否为空
 def check_string(**kw):
     for key, string in kw.items():
         if not string or not string.strip():
             abort(400, '%s cannot be empty.' % key)
+
+
+# 检查邮箱和密码的格式是否合法
+def check_email_and_password(email, password):
+    if not email or not _RE_EMAIL.match(email):
+        abort(400, 'Invalid email')
+    if not password or not _RE_SHA1.match(password):
+        abort(400, 'Invalid password')
